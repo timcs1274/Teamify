8a05b83749a2aa3132ce5bdceafa39118be93ba5
timcs <timcs1274@gmail.com>
2022-07-21 22:59:43 -0400

    finished html template

--- src/htmlTemp.js
+++ src/htmlTemp.js
@@ -70,6 +70,29 @@ const generateTeam = team => {
     
 return htmlArr.join('');
 
-
-
 }
+
+module.exports = team => {
+    return `
+    <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href = "style.css">
+  </head>
+<header>
+    <h1>My Team</h1>
+</header>
+  <body>
+    <div class="container px-4 text-center">
+        <div class="row gx-5">
+          ${generateTeam(team)}
+    <script src="/index.js"></script>
+  </body>
+
+</html>
+    `
+};
